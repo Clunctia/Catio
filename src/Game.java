5444440a443f15e92e0294ee35dfd7dfa53d18c7
sorawish <sorawish110739@gmail.com>
2017-05-24 14:47:12 +0700

    add FPS on console

--- src/Game.java
+++ src/Game.java
@@ -3,12 +3,66 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas{
+public class Game extends Canvas implements Runnable {
 	
 	public static final int WIDTH = 270;
 	public static final int HEIGHT = WIDTH/14*10;
 	public static final int SCALE = 4;
 	public static final String TITLE = "Mario";
+	
+	private Thread thread;
+	private boolean running = false;
+	
+	public synchronized void start(){
+		if(running)return;
+		running = true;
+		thread = new Thread(this,"Thread");
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		if(!running)return;
+		running = false;
+		try{
+			thread.join();
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void run(){
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		double delta = 0.0;
+		double ns = 100000000/60.0;
+		int frames = 0;
+		int ticks = 0;
+		while(running){
+			long now = System.nanoTime();
+			delta+=(now-lastTime)/ns;
+			lastTime = now;
+			while(delta>=1){
+				tick();
+				ticks++;
+				delta--;
+			}
+			render();
+			frames++;
+			if(System.currentTimeMillis()-timer>1000){
+				timer+=1000;
+				System.out.println(frames + " FPS " + ticks + " UPS ");
+				frames = 0;
+				ticks = 0;
+			}
+		}stop();
+	}
+	public void render(){
+		
+	}
+	
+	public void tick(){
+		
+	}
 	
 	public Game(){
 		Dimension size = new Dimension(WIDTH*SCALE,HEIGHT*SCALE);
@@ -25,6 +79,9 @@ public class Game extends Canvas{
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		game.start();
 	}
+
+	
 }
 	
