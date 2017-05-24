e26d2bf7b9ec6c401d4789f24409b4d58532e009
sorawish <sorawish110739@gmail.com>
2017-05-24 12:43:09 +0700

    Canvas Dimension

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,17 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-public class Game {
+public class Game extends Canvas{
 	
 	public static final int WIDTH = 270;
 	public static final int HEIGHT = WIDTH/14*10;
 	public static final int SCALE = 4;
 	
+	public Game(){
+		Dimension size = new Dimension(WIDTH*SCALE,HEIGHT*SCALE);
+		setPreferredSize(size);
+		setMaximumSize(size);
+		setMinimumSize(size);
+	}
 }
+	
